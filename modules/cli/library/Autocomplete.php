9de05baf7e52261aab32b9d63918c36d314a3357
Iqbal Fauzi <iqbalfawz@gmail.com>
2018-07-27 08:00:47 +0700

    autocomplete?

--- modules/cli/library/Autocomplete.php
+++ modules/cli/library/Autocomplete.php
@@ -9,35 +9,35 @@ namespace Cli\Library;
 
 class Autocomplete extends \Cli\Autocomplete
 {
-	static function none(): string{
-		return '1';
-	}
+    static function none(): string{
+        return '1';
+    }
 
-	static function primary($args): string{
-		$farg = $args[0];
+    static function primary($args): string{
+        $farg = $args[0];
 
-		$gates  = include BASEPATH . '/etc/cache/gates.php';
-		$routes = include BASEPATH . '/etc/cache/routes.php';
+        $gates  = include BASEPATH . '/etc/cache/gates.php';
+        $routes = include BASEPATH . '/etc/cache/routes.php';
 
-		$result = [];
+        $result = [];
 
-		foreach($gates as $gate){
+        foreach($gates as $gate){
             if($gate->host->value !== 'CLI')
                 continue;
 
             foreach($routes->{$gate->name} as $route){
                 $bpath = explode(' ', trim($route->path->value))[0];
                 if($bpath === 'autocomplete')
-                	continue;
+                    continue;
 
                 if(!in_array($bpath, $result))
-                	$result[] = $bpath;
+                    $result[] = $bpath;
             }
         }
         
         if($farg === '-')
-        	return trim(implode(' ', $result));
+            return trim(implode(' ', $result));
 
         return parent::lastArg($farg, $result);
-	}
+    }
 }
