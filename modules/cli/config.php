3d27a91d648dcc85be6b01744302e9cf5c4cc6a2
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-09-08 20:01:28 +0700

    Not print command result on exec request

--- modules/cli/config.php
+++ modules/cli/config.php
@@ -7,7 +7,7 @@
 
 return [
     '__name' => 'cli',
-    '__version' => '0.3.0',
+    '__version' => '0.3.1',
     '__git' => 'git@github.com:getphun/cli.git',
     '__license' => 'MIT',
     '__author' => [
